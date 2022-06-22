defed95996f856415cacc841c65d02dd4ed7d1f4
Taro Mieno <tmieno2@unl.edu>
2022-06-22 06:12:14 -0500

    update

--- knit_help.R
+++ knit_help.R
@@ -9,22 +9,24 @@ library(xaringanBuilder)
 # /*===========================================================
 
 # === prediction v.s. causal ===#
-quarto::quarto_render(here::here("LectureNotes/L00-prediction-vs-causal-inference.qmd"))
+quarto::quarto_render(here::here("LectureNotes/B00-prediction-vs-causal-inference.qmd"))
+
+# === non-linear ===#
+quarto::quarto_render(here::here("LectureNotes/B01-nonlinear.qmd"))
 
 # === bias-variance trade-off ===#
-quarto::quarto_render(here::here("LectureNotes/bias-variance-tradeoff.qmd"))
-
-# === gam ===#
-quarto::quarto_render(here::here("LectureNotes/gam.qmd"))
-
-# === cross-validation ===#
-quarto::quarto_render(here::here("LectureNotes/cross-validation.qmd"))
-
-# === bootstrap ===#
-quarto::quarto_render(here::here("LectureNotes/bootstrap.qmd")) # === bootstrap ===#
+quarto::quarto_render(here::here("LectureNotes/B02-bias-variance-tradeoff.qmd"))
 
 # === LASSO ===#
-quarto::quarto_render(here::here("LectureNotes/lasso.qmd"))
+quarto::quarto_render(here::here("LectureNotes/B03-regularization.qmd"))
+
+# === cross-validation ===#
+quarto::quarto_render(here::here("LectureNotes/B04-cross-validation.qmd"))
+
+# === bootstrap ===#
+quarto::quarto_render(here::here("LectureNotes/B05-bootstrap.qmd")) # === bootstrap ===#
+
+
 
 # === Tree-based ===#
 quarto::quarto_render(here::here("LectureNotes/tree-based-ml.qmd"))
